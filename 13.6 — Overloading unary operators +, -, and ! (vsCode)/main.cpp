8905535dca52dad4e6e7ceb4caa3bddc23d3c2eb
Mateusz <oureyelet@gmail.com>
2022-01-21 21:12:40 +0000

    -

--- 13.6 — Overloading unary operators +, -, and ! (vsCode)/main.cpp
+++ 13.6 — Overloading unary operators +, -, and ! (vsCode)/main.cpp
@@ -39,6 +39,62 @@ Cents Cents::operator+() const
     return {+m_cents}; 
 }
 
+class Point
+{
+private:
+    double m_x{};
+    double m_y{};
+    double m_z{};
+
+public:
+    Point(double x=0.0, double y=0.0, double z=0.0)
+        :   m_x{ x }, m_y{ y }, m_z{ z }
+    {
+    }
+
+
+    // Convert a Point into its negative equivalent
+    Point operator- () const;
+
+    // Convert a Point into its negative equivalent
+    Point operator+ () const;
+
+    // Return true if the point is set at the origin
+    bool operator! () const;
+
+    //overloading output friend function <<
+    friend std::ostream& operator<<(std::ostream& out,const Point& point);
+
+
+    double get_X() const { return m_x; }
+    double get_Y() const { return m_y; }
+    double get_Z() const { return m_z; }
+};
+
+// Convert a Point into its negative equivalent
+Point Point::operator- () const
+{
+    return Point { -m_x, -m_y, -m_z }; 
+}
+
+// Return true if the point is set at the origin, false otherwise
+bool Point::operator! () const
+{
+    return (m_x == 0.0 && m_y == 0.0 && m_z == 0.0);
+}
+
+Point Point::operator+ () const
+{
+    return { +m_x, +m_y, +m_z };
+}
+
+std::ostream& operator<<(std::ostream& out,const Point& point)
+{
+    out << point.get_X() << point.get_Y() << point.get_Z();
+
+    return out;
+}
+
 int main()
 {
     std::cout << "Hello World" << '\n';
@@ -75,19 +131,55 @@ int main()
     */
 
     std::string answer{};
-
+/*
     while (answer != "happy")
     {
         std::cout << "How are you today?";
     
         std::cin >> answer;
     }
-
+*/
     if(answer != "happy")
         std::cout << ":(" << '\n';
     else
         std::cout << ":)" << '\n';
     
+    /*
+    For integers, 0 evaluates to false, and anything else to true, so operator! as applied to integers will return true 
+    for an integer value of 0 and false otherwise.
+
+    Extending the concept, we can say that operator! should evaluate to true if the state of the object is “false”, “zero”, 
+    or whatever the default initialization state is.
+
+    The following example shows an overload of both operator- and operator! for a user-defined Point class:
+    */
+    Point point{3.3};
+
+    if(!point)
+        std::cout << "point is set at the origin.\n";
+    else   
+        std::cout << "point is not set at the origin.\n";
+
+    /*
+    The overloaded operator! for this class returns the Boolean value “true” if the Point is set to the 
+    default value at coordinate (0.0, 0.0, 0.0). Thus, the above code produces the result:
+
+    point is set at the origin.
+    */
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Quiz time" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    Implement overloaded operator+ for the Point class.
+    */
+    Point point2{ 3.3, -7.7, -33.9 };
+
+    std::cout << (+point2);
 
 
     return 0;
